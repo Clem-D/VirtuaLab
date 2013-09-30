38bf3d3dd577aa8e93d51ac1265facee666786d7
Clem-D <cclementddel@gmail.com>
2013-09-30 14:02:49 +0200

    Update help_session.php
    update

--- utils/help_session.php
+++ utils/help_session.php
@@ -1,9 +1,8 @@
 <?php
-$dureelimite=20*1;
+$dureelimite=20*1; // period max
 
     if (time() - $_SESSION['derniereaction'] > $dureelimite) {
         echo "Session vaut : " . $_SESSION['derniereaction']." et limite : ".$dureelimite;
-        //header('Location: ../utils/timeout.php');
     }
     else {
         $_SESSION['derniereaction'] = time();
